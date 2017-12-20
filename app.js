efd6aeb84abe1ed8048d28a18b5555ffbe312f97
Tyler Mustard <tjm@Tylers-MacBook-Air.local>
2017-12-20 12:54:11 -0700

    commit

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-const apiURL = ''
+const apiURL = 'https://fast-cove-95183.herokuapp.com/';
 var occasionData
 var occasionSelected
 
-fetch(apiUrl)
+fetch(apiURL)
 .then(response => response.json())
 .then(response => occasionData = response)
 
 function setOccasion(sel) {
-  console.log('its work');
-  console.log(selection);
-  occasionSelected = sel.options[sel.selectedIndex].text
-  console.log(occasionSelected);
+ occasionSelected = sel.options[sel.selectedIndex].text;
 }
+
+document.querySelector("#dropD").addEventListener('submit', function(event){
+ console.log('working')
+ event.preventDefault();
+ populateDescription();
+});
+
+function populateDescription () {
+ var descriptionP = document.querySelector(".description-content")
+ console.log(descriptionP)
+ occasionData.map(item => {
+   if (item.occasion === occasionSelected) {
+     var outfitDesc = item.outfitDescription
+     descriptionP.innerHTML = outfitDesc
+     var wolfImage = document.querySelector(".wolf-image")
+     wolfImage.src = item.image
+     for (var i = 0; i < item.accDescription.length; i++) {
+       var $li = document.createElement('li')
+       var $ul = document.querySelector('.accessories-list')
+       $li.innerHTML = item.accDescription[i]
+       $ul.appendChild($li)
+     }
+   }
+ })
+};
